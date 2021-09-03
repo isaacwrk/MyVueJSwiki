a8dcee431fe53fa7c5358419bd8bdb7e5a60b1d7
isaacwrk <isaacsouzawrk@gmail.com>
2021-09-03 10:03:46 -0300

    transitions with keyframes and default write class

--- transitions/src/App.vue
+++ transitions/src/App.vue
@@ -3,8 +3,13 @@
 		<h1>Animações</h1>
 		<hr>
 		<b-button variant="primary" class="mb-4" @click="exibir =!exibir">Mostrar mensagem</b-button>
-		<transition>
-			<b-alert variant="info" show v-if="exibir"> {{msg}} </b-alert>
+
+		<transition name='fade'>
+			<b-alert variant="info" show v-if="exibir"> {{msg}} Fade </b-alert>
+		</transition>
+
+		<transition name='slide'>
+			<b-alert variant="info" show v-if="exibir"> {{msg}} Slide </b-alert>
 		</transition>
 	</div>
 </template>
@@ -32,4 +37,27 @@ export default {
 	margin-top: 60px;
 	font-size: 1.5rem;
 }
+
+.fade-enter , .fade-leave-to{
+	opacity: 0;
+}
+
+.fade-enter-active, .fade-leave-active{
+	transition: opacity 1.5s;
+}
+
+@keyframes slide-in {
+	from {transform: translateY(40px);}
+	to{transform: translateY(0);}
+}
+
+@keyframes slide-out{
+	from{transform: translateY(0);}
+	to{transform: translateY(40px);}
+}
+
+.slide-enter-active{
+	animation:slide-in 2s ease;
+}
+
 </style>
