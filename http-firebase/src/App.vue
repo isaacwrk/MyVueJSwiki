989796868ca6a8c3f70f6485cb29cdd1ded2eb8a
isaacwrk <isaacsouzawrk@gmail.com>
2021-09-14 09:00:31 -0300

    adding messages

--- http-firebase/src/App.vue
+++ http-firebase/src/App.vue
@@ -1,27 +1,37 @@
 <template>
 	<div id="app" class="container">
 		<h1>HTTP com Axios</h1>
+		<b-alert show dismissible v-for="mensagem in mensagens"
+			:key="mensagem.texto"
+			:variant="mensagem.tipo">{{ mensagem.texto }}</b-alert>
 		<b-card>
 			<b-form-group label="Nome:">
-				<b-form-input type="text" size="lg" v-model="usuario.nome" placeholder="Informe o Nome">
-				</b-form-input>
+				<b-form-input type="text" size="lg"
+					v-model="usuario.nome"
+					placeholder="Informe o Nome"></b-form-input>
 			</b-form-group>
 			<b-form-group label="E-mail:">
-				<b-form-input type="email" size="lg" v-model="usuario.email" placeholder="Informe o Email">
-				</b-form-input>
+				<b-form-input type="email" size="lg"
+					v-model="usuario.email"
+					placeholder="Informe o E-mail"></b-form-input>
 			</b-form-group>
 			<hr>
-			<b-button @click="salvar" size="lg" variant="primary">Salvar</b-button>
-			<b-button @click.prevent="obterUsuarios" size="lg" variant="success" class="ml-2">Obter Usuários</b-button>
+			<b-button @click="salvar"
+				size="lg" variant="primary">Salvar</b-button>
+			<b-button @click="obterUsuarios"
+				size="lg" variant="success"
+				class="ml-2">Obter Usuários</b-button>
 		</b-card>
 		<hr>
 		<b-list-group>
-			<b-list-group-item v-for="(usuario,id) in usuarios" :key="id">
-				<strong>Nome:</strong>{{ usuario.nome }}<br>
-				<strong>E-mail</strong>{{ usuario.email }}<br>
-				<strong>ID:</strong>{{ id }}<br>
-				<b-button variant="warning" size="lg" @click="carregar(id)">Carregar</b-button>
-				<b-button variant="danger" size="lg" class="ml-2" @click="excluir(id)">Excluir</b-button>
+			<b-list-group-item v-for="(usuario, id) in usuarios" :key="id">
+				<strong>Nome: </strong> {{ usuario.nome }}<br>
+				<strong>E-mail: </strong> {{ usuario.email }}<br>
+				<strong>ID: </strong> {{ id }}<br>
+				<b-button variant="warning" size="lg"
+					@click="carregar(id)">Carregar</b-button>
+				<b-button variant="danger" size="lg" class="ml-2"
+					@click="excluir(id)">Excluir</b-button>
 			</b-list-group-item>
 		</b-list-group>
 	</div>
@@ -29,38 +39,64 @@
 
 <script>
 export default {
-	data(){
-		return{
-			usuarios:[],
-			id:null,
-			usuario:{
-				nome:'',
-				email:''
+	data() {
+		return {
+			mensagens: [],
+			usuarios: [],
+			id: null,
+			usuario: {
+				nome: '',
+				email: ''
 			}
 		}
 	},
-	methods:{
-		limpar(){
-			this.usuario.nome = '',
-			this.usuario.email = '',
+	methods: {
+		limpar() {
+			this.usuario.nome = ''
+			this.usuario.email = ''
 			this.id = null
+			this.mensagens = []
 		},
-		carregar(id){
-			this.id = id,
-			this.usuario = {...this.usuarios[id]}
+		carregar(id) {
+			this.id = id
+			this.usuario = { ...this.usuarios[id] }
 		},
-		excluir(id){
-			this.$http.delete(`/usuarios/${id}.json`).then(()=> this.limpar())
+		excluir(id) {
+			this.$http.delete(`/usuarios/${id}`)
+				.then(() => this.limpar())
+				.catch(err => {
+					this.limpar()
+					this.mensagens.push({
+						texto: 'Problema para excluir!',
+						tipo: 'danger'
+					})
+				})
 		},
-		salvar(){
-			this.$http.post('usuarios.json', this.usuario).then(()=> this.limpar())
+		salvar() {
+			const metodo = this.id ? 'patch' : 'post'
+			const finalUrl = this.id ? `/${this.id}.json` : '.json'
+			this.$http[metodo](`/usuarios${finalUrl}`, this.usuario)
+				.then(_ => {
+					this.limpar()
+					this.mensagens.push({
+						texto: 'Operação realizada com sucesso!',
+						tipo: 'success'
+					})
+				})
 		},
-		obterUsuarios(){
-			this.$http.get('usuarios.json').then(res =>{
+		obterUsuarios() {
+			this.$http.get('usuarios.json').then(res => {
 				this.usuarios = res.data
+				console.log(res.data)
 			})
 		}
 	}
+	// created() {
+	// 	this.$http.post('usuarios.json', {
+	// 		nome: 'Maria',
+	// 		email: 'maria_maria@gmail.com'
+	// 	}).then(res => console.log(res))
+	// }
 }
 </script>
 
