18fe0a79c60e4cf3801fe663d04c90dc6b5a4bd7
Isaac Souza <isaacsouzawrk@gmail.com>
2021-09-16 10:55:53 -0300

    creating routes with javascript

--- rotas-exercicios/src/components/usuario/Usuario.vue
+++ rotas-exercicios/src/components/usuario/Usuario.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
       <h2>Usuário</h2>
+      <hr>
+      <button sucesso @click="irParaInicio">Voltar</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods:{
+    irParaInicio(){
+      this.$router.push('/')
+    }
+  }
 }
 </script>
 
