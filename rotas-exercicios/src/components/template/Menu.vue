b1bff20c058f962edbfccf45d74e12f27d49d266
Isaac Souza <isaacsouzawrk@gmail.com>
2021-09-16 10:45:38 -0300

    creating Menu.vue file and router-link

---
+++ rotas-exercicios/src/components/template/Menu.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav class="menu">
+        <router-link to='/'>Início</router-link>
+        <router-link to='/usuario'>usuario</router-link>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
