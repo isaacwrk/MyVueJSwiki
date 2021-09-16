141719364708b2e3f125bdcfd7269c4953b9d2b5
Isaac Souza <isaacsouzawrk@gmail.com>
2021-09-16 11:32:18 -0300

    creating more router-link

--- rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
+++ rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
@@ -3,6 +3,9 @@
     <h3>Usuário Detalhe</h3>
     <hr>
     <p><strong>Codigo:</strong>{{id}}</p>
+    <router-link tag="button" primario :to="`/usuario/${id}/editar`">
+        Editar
+    </router-link>
   </div>
 </template>
 
--- rotas-exercicios/src/components/usuario/UsuarioEditar.vue
+++ rotas-exercicios/src/components/usuario/UsuarioEditar.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="usuario-editar">
-        <h3>Usuario Editar</h3>
+    <h3>Usuario Editar</h3>
+    <hr>
+    <p><strong>Codigo:</strong>{{id}}</p>
   </div>
 </template>
 
 <script>
 export default {
-
+    props:['id']
 }
 </script>
 
